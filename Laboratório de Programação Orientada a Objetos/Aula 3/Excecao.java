afeb9c3e6b65184a3bd1b1b25b0fb5d9eecc9c34
Paulo André Pimenta Aragão <andrepimenta@r09l09.cl.ibilce.unesp.br>
2019-05-08 15:23:32 -0300

    Aula 4 - Laboratório

--- Laboratório de Programação Orientada a Objetos/Aula 3/Excecao.java
+++ Laboratório de Programação Orientada a Objetos/Aula 3/Excecao.java
@@ -4,17 +4,21 @@ import java.util.*;
 public class Excecao{
     public static void main(String args[]){
         Scanner d = new Scanner(System.in);
+
         try{
             int n = 0;
             System.out.print("Qual o tamanho do vetor: ");
             n = d.nextInt(); // Tamanho do vetor
             int[] v = new int[n]; // Cria o vetor, se n for negativo cai no primeiro catch
+
             for(int i = 0; i < n; i++){
                 System.out.print("Insira um numero: ");
                 v[i] = d.nextInt(); // Insere valores no vetor
             }
+
             System.out.print("\n1 - Dividir todo o vetor por um numero\n2 - Raiz quadrada de cada elemento do vetor\nSua opcao: ");
-            int op = d.nextInt();            
+            int op = d.nextInt();
+
             switch(op){
                 case 1:
                     System.out.print("\nInsira um numero para dividir todo o vetor: ");
@@ -26,23 +30,27 @@ public class Excecao{
                         System.out.println(v[i] + "/" + num + " = " + (double) v[i] / num); // Mostra os números do vetor dividos
                     }
                     break;
+
                 case 2:
                     for(int i = 0; i < n; i++){
                         if(v[i] < 0)
                             throw new ArithmeticException("\nRaiz quadrada de numero negativo!");
-                        System.out.println("Raiz quadrada de " + v[i] + ": " +Math.sqrt(v[i]));
+                        System.out.println("Raiz quadrada de " + v[i] + ": " +Math.sqrt(v[i])); // Mostra a raíz quadrada
                     }
                     break;
                 default:
                     System.out.println("\nOpcao invalida!");             
             }      
         }
+        
         catch(NegativeArraySizeException e){ // Caso o índice do vetor seja negativo
             System.out.println("Indice negativo!");
         }
+        
         catch(InputMismatchException e){ // Caso seja inserido um caracter ao invéz de um número
             System.out.println("Insira somente números!");
         }
+        
         catch(ArithmeticException e){ // Caso o denominador seja 0
             System.out.println(e.getMessage());
         }
--- Laboratório de Programação Orientada a Objetos/Aula 4/Item/Carrinho.java
+++ Laboratório de Programação Orientada a Objetos/Aula 4/Item/Carrinho.java
@@ -1,44 +1,61 @@
 import java.util.Scanner;
+import javax.swing.JOptionPane;
 
 public class Carrinho{
     public static void main(String args[]){
-        Scanner d = new Scanner(System.in);
-        String[] id = new String[4];
-        String[] description = new String[4];
-        int[] quantity = new int[4];
-        double[] unity_price = new double[4];
-        double total = 0.0;
+        try{
+            String[] id = new String[4]; // Vetor para o ID
+            String[] description = new String[4]; // Vetor para a descrição
+            int[] quantity = new int[4]; // Vetor quantidade
+            double[] unity_price = new double[4]; // Vetor para preço unitário
+            double total = 0.0; // Valor total da compra
 
-        for(int i = 0; i < 4; i++){
-            System.out.print("ID: ");
-            id[i] = d.nextLine();
-            d = new Scanner(System.in);
-            System.out.print("Descricao: ");
-            description[i] = d.nextLine();
-            d = new Scanner(System.in);
-            System.out.print("Quantidade: ");
-            quantity[i] = d.nextInt();
-            System.out.print("Preco: ");
-            unity_price[i] = d.nextDouble();
-            d = new Scanner(System.in);
-            System.out.println();
+            for(int i = 0; i < 4; i++){ // Lê os valores para serem inseridos
+                JOptionPane.showMessageDialog(null, "Inserção do produto " + (i + 1), "Produto " + (i + 1), JOptionPane.INFORMATION_MESSAGE);
+
+                id[i] = JOptionPane.showInputDialog(null, "ID:", "Produto " + (i + 1), JOptionPane.QUESTION_MESSAGE); // ID
+                
+                description[i] = JOptionPane.showInputDialog(null, "Descrição:", "Produto " + (i + 1), JOptionPane.QUESTION_MESSAGE); // Descrição
+                
+                quantity[i] = Integer.parseInt(JOptionPane.showInputDialog(null, "Quantidade:", "Produto " + (i + 1), JOptionPane.QUESTION_MESSAGE)); // Quantidade
+
+                unity_price[i] = Double.parseDouble(JOptionPane.showInputDialog(null, "Preço:", "Produto " + (i + 1), JOptionPane.QUESTION_MESSAGE)); // Preço
+            }
+
+            Item item1 = new Item(id[0], description[0], quantity[0], unity_price[0]); // Insere os valores do item1
+            Item item2 = new Item(id[1], description[1], quantity[1], unity_price[1]); // Insere os valores do item2
+            Item item3 = new Item(id[2], description[2], quantity[2], unity_price[2]); // Insere os valores do item3
+            Item item4 = new Item(id[3], description[3], quantity[3], unity_price[3]); // Insere os valores do item4
+            double disc = (Double.parseDouble(JOptionPane.showInputDialog(null, "Quanto de disconto deseja inserir (em %):"))) / 100;
+            int op = Integer.parseInt(JOptionPane.showInputDialog(null,"Qual item deseja por o desconto?", "Desconto", JOptionPane.QUESTION_MESSAGE));
+            switch(op){
+                case 1:
+                    item1.setDiscount(disc); // Dá um desconto para o item1
+                    break;
+                case 2:
+                    item2.setDiscount(disc); // Dá um desconto para o item2
+                    break;
+                case 3:
+                    item3.setDiscount(disc); // Dá um desconto para o item3
+                    break;
+                case 4:
+                    item4.setDiscount(disc); // Dá um desconto para o item4
+                    break;
+                default:
+                    item3.setDiscount(disc); // Dá um desconto para o item3
+            }
+
+            total = item1.getAdjustedTotal() + item2.getAdjustedTotal() + item3.getAdjustedTotal() + item4.getAdjustedTotal(); // Soma o total dos itens
+
+            JOptionPane.showMessageDialog(null,
+              "Item 1\n" + "Descricao: " + item1.getDescription() + "\nQuantidade: " + item1.getQuantity() + "\nPreco: " + item1.getPriceWithDiscount()
+            + "\n\nItem 2\n" + "Descricao: " + item2.getDescription() + "\nQuantidade: " + item2.getQuantity() + "\nPreco: " + item2.getPriceWithDiscount()
+            + "\n\nItem 3\n" + "Descricao: " + item3.getDescription() + "\nQuantidade: " + item3.getQuantity() + "\nPreco: " + item3.getPriceWithDiscount()
+            + "\n\nItem 4\n" + "Descricao: " + item4.getDescription() + "\nQuantidade: " + item4.getQuantity() + "\nPreco: " + item4.getPriceWithDiscount()
+            + "\n\nTotal: " + total, "Nota Fiscal", JOptionPane.INFORMATION_MESSAGE); // Nota Fiscal
         }
-
-        Item item1 = new Item(id[0], description[0], quantity[0], unity_price[0]);
-        Item item2 = new Item(id[1], description[1], quantity[1], unity_price[1]);
-        Item item3 = new Item(id[2], description[2], quantity[2], unity_price[2]);
-        Item item4 = new Item(id[3], description[3], quantity[3], unity_price[3]);
-
-        total += item1.getAdjustedTotal();
-        total += item2.getAdjustedTotal();
-        total += item3.getAdjustedTotal();
-        total += item4.getAdjustedTotal();
-
-        item3.setDiscount(0.15);
-        System.out.println("Descricao: " + item1.getDescription() + " - Quantidade: " + item1.getQuantity() + "  - Preco: " + item1.getPrice());
-        System.out.println("Descricao: " + item2.getDescription() + " - Quantidade: " + item2.getQuantity() + "  - Preco: " + item2.getPrice());
-        System.out.println("Descricao: " + item3.getDescription() + " - Quantidade: " + item3.getQuantity() + "  - Preco: " + item3.getPriceWithDiscount());
-        System.out.println("Descricao: " + item4.getDescription() + " - Quantidade: " + item4.getQuantity() + "  - Preco: " + item4.getPrice());
-        System.out.println("Total: " + total);
+        catch(NumberFormatException e){
+            JOptionPane.showMessageDialog(null, e.getMessage(), "Erro", JOptionPane.ERROR_MESSAGE); // Caso insira letras ao invéz de números
+        }
     }
 }
